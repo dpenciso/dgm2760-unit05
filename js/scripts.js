2c06d6b866b11f7f6db64d44adcb446bbdc97662
David Enciso <dpeniciso@msn.com>
2020-02-10 13:21:50 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// create an array with 4 trees listed
+const trees = ['oak', 'Pine', 'aspen', 'Bald Cypress']
+
+const errorElement = document.querySelector('#error')
+
+const displayResults = document.querySelector('#displayResults')
+
+// display the list of trees inside the displayResults div
+const listTrees = () => {
+    let treeList = ''
+    trees.forEach(tree => {
+        treeList += `${tree} <br>`
+    })
+    displayResults.innerHTML = `${treeList} <span>${trees.length} elements long</span>`
+}
+
+listTrees()
+
+//concise version of add a redwood to the end
+document.querySelector('#add_redwood').onclick = () => {
+    trees.push('redwood')
+    listTrees()
+}
+
+//add pear to the end
+document.querySelector('#add_pear').onclick = () => {
+    trees.unshift('Pear')
+    listTrees()
+}
+
+document.querySelector('#remove_tree1').onclick = () => {
+    if (trees.length > 0) {
+        trees.shift()
+        listTrees()
+    } else {
+        errorElement.textContent = 'I cannot remove the first tree cause there are NO TREES'
+    }
+}
